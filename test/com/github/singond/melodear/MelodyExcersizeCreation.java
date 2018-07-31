17d0ea1774a09e8073a3717a44a51160fe1de26f
Jan Singon Slany <slanyj@centrum.cz>
2018-08-01 00:00:10 +0200

    Test creating melody excersize

---
+++ test/com/github/singond/melodear/MelodyExcersizeCreation.java
@@ -0,0 +1,24 @@
+package com.github.singond.melodear;
+
+import static com.github.singond.music.PitchClass.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.github.singond.music.Pitch;
+import com.github.singond.music.PitchClass;
+
+public class MelodyExcersizeCreation {
+
+	@Test
+	public void createMelody() {
+		Set<PitchClass> pcs = new HashSet<>(Arrays.asList(D, E, F_SHARP, G, A, B, C_SHARP));
+		Pitch lowest = Pitch.of(C, 3);
+		Pitch highest = Pitch.of(C, 5);
+		MelodyExcersize exc = MelodyExcersize.randomWithPitchClasses(pcs, lowest, highest, 4);
+		System.out.format("Created random melody excersize: %s", exc);
+	}
+}
