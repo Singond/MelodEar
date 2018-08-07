7a41d1e7dd24e85eaeb4777177c46d5a79d8883b
Jan Singon Slany <slanyj@centrum.cz>
2018-08-07 16:01:23 +0200

    Add main activity before keyboard

--- melodear-android/src/main/java/com/github/singond/melodear/android/MainActivity.java
+++ melodear-android/src/main/java/com/github/singond/melodear/android/MainActivity.java
@@ -1,12 +1,20 @@
 package com.github.singond.melodear.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void startMelodicDictation(@SuppressWarnings("unused") View view) {
+		Intent intent = new Intent(this, MelodicDictationActivity.class);
+		startActivity(intent);
+	}
 }
