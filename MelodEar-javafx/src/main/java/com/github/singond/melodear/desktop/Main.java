d9f08ff8c71eb056ee7f5d6522d03714345b1a66
Jan Singon Slany <slanyj@centrum.cz>
2019-06-09 11:03:07 +0200

    Separate view into master and included part

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/Main.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/Main.java
@@ -6,26 +6,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(
+				getClass().getResource("/view/main.fxml"));
+		BorderPane root = (BorderPane) loader.load();
+		Scene scene = new Scene(root);
+		root.setCenter(pianoPane());
+
 		stage.setTitle("MelodEar");
-		stage.setScene(pianoScene());
+		stage.setScene(scene);
 		stage.show();
 	}
 
-	private Scene pianoScene() throws IOException {
+	private Parent pianoPane() throws IOException {
 		FXMLLoader loader = new FXMLLoader(
 				getClass().getResource("/view/piano/piano.fxml"));
 		loader.setController(DaggerMainComponent.create().getPianoController());
-		Parent root = loader.load();
-
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add("/view/piano/piano.css");
-		return scene;
+		Parent pane = loader.load();
+		pane.getStylesheets().add("/view/piano/piano.css");
+		return pane;
 	}
 
 	public static void main(String[] args) {
