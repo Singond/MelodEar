46c7df8c107b48b099dfd4f95a1883ed94f5a499
Jan Singon Slany <slanyj@centrum.cz>
2019-06-09 19:51:53 +0200

    Remove unused Dagger entry point

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/MainComponent.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/MainComponent.java
@@ -4,13 +4,10 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 
-import com.github.singond.melodear.desktop.piano.PianoController;
-
 @Singleton
 @Component(modules = AudioModule.class)
 interface MainComponent {
 
 	MainController getMainController();
-	PianoController getPianoController();
 
 }
