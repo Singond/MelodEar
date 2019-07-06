d899e67a754d0d14d8a73bc3de4746ae90ef1113
Jan Singon Slany <slanyj@centrum.cz>
2019-07-06 19:07:18 +0200

    Extract abstract class from settings view

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/keyboard/KeyboardSettingsView.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/keyboard/KeyboardSettingsView.java
@@ -1,42 +1,20 @@
 package com.github.singond.melodear.desktop.keyboard;
 
-import java.io.IOException;
-import java.util.ResourceBundle;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-
 import com.github.singond.melodear.desktop.settings.AllSettings;
+import com.github.singond.melodear.desktop.settings.BasicSettingsView;
 import com.github.singond.melodear.desktop.settings.SettingsView;
 
-public class KeyboardSettingsView implements SettingsView<KeyboardSettings> {
+public class KeyboardSettingsView
+		extends BasicSettingsView<KeyboardSettings>
+		implements SettingsView<KeyboardSettings> {
 
-	private static final ResourceBundle bundle
-			= ResourceBundle.getBundle("loc/settings");
-
-	@Override
-	public Node getNode(AllSettings s) {
-		FXMLLoader loader = new FXMLLoader(
-				getClass().getResource("/view/settings_keyboard.fxml"), bundle);
-		loader.setController(new KeyboardSettingsController(s.keyboard()));
-		try {
-			Parent node = (Parent) loader.load();
-			node.getStylesheets().add("/view/settings_keyboard.css");
-			return node;
-		} catch (IOException e) {
-			throw new RuntimeException("Could not load view/settings_keyboard.fxml", e);
-		}
+	public KeyboardSettingsView() {
+		super("keyboard");
 	}
 
 	@Override
-	public String getName() {
-		return bundle.getString("keyboard.title");
-	}
-
-	@Override
-	public String toString() {
-		return "keyboard settings";
+	protected Object makeController(AllSettings s) {
+		return new KeyboardSettingsController(s.keyboard());
 	}
 
 }
