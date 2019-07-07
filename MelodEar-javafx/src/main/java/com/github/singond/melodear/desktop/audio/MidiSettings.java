36cf5c4813d2e3db1fc94253f133c069f26b6fca
Jan Singon Slany <slanyj@centrum.cz>
2019-07-07 11:24:02 +0200

    Remove MIDI settings initialization from controller

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/audio/MidiSettings.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/audio/MidiSettings.java
@@ -1,16 +1,24 @@
 package com.github.singond.melodear.desktop.audio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.github.singond.melodear.desktop.settings.Settings;
 
-public class MidiSettings implements Settings {
+public final class MidiSettings implements Settings {
 
 	private static Logger logger = LogManager.getLogger(MidiSettings.class);
 
@@ -21,6 +29,7 @@ public class MidiSettings implements Settings {
 
 	public MidiSettings() {
 		logger.debug("Creating MidiSettings");
+		initSynth();
 		synth.addListener((v, o, n) ->
 				logger.debug("Synthesizer changed from {} to {}", o, n));
 	}
@@ -39,6 +48,42 @@ public class MidiSettings implements Settings {
 
 	public ObjectProperty<MidiDevice.Info> synthProperty() {
 		return synth;
+	}
+
+	public final ObservableList<MidiDevice.Info> getAvailableSynthesizers() {
+		return FXCollections.observableArrayList(synthesizers());
+	}
+
+	private void initSynth() {
+		logger.debug("Initializing synthesizer settings");
+
+		// Detect current synthesizer
+		List<MidiDevice.Info> synths = synthesizers();
+		try (Synthesizer currentSynth = MidiSystem.getSynthesizer()) {
+			if (synths.contains(currentSynth.getDeviceInfo())) {
+				synth.set(currentSynth.getDeviceInfo());
+			} else {
+				logger.warn("Cannot find current synthesizer ({}) among"
+						+ "available devices ({})", currentSynth, synths);
+			}
+		} catch (MidiUnavailableException e) {
+			logger.error("Could not obtain current synthesizer", e);
+		}
+	}
+
+	private List<MidiDevice.Info> synthesizers() {
+		List<MidiDevice.Info> result = new ArrayList<>();
+		for (MidiDevice.Info info : MidiSystem.getMidiDeviceInfo()) {
+			logger.debug("MIDI device: {}", info);
+			try (MidiDevice device = MidiSystem.getMidiDevice(info)) {
+				if (device instanceof Synthesizer) {
+					result.add(info);
+				}
+			} catch (MidiUnavailableException e) {
+				logger.error("Could not obtain device " + info, e);
+			}
+		}
+		return result;
 	}
 
 	private static void copyFields(MidiSettings src, MidiSettings tgt) {
--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/audio/MidiSettingsController.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/audio/MidiSettingsController.java
@@ -1,15 +1,8 @@
 package com.github.singond.melodear.desktop.audio;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Synthesizer;
 
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -47,20 +40,10 @@ public class MidiSettingsController {
 
 	private void initSynth() {
 		logger.debug("Initializing synthesizer list");
+		synth.setItems(settings.getAvailableSynthesizers());
+		synth.getSelectionModel().select(settings.getSynth());
 		settings.synthProperty().bind(
 				synth.getSelectionModel().selectedItemProperty());
-		List<MidiDevice.Info> synths = synthesizers();
-		synth.setItems(FXCollections.observableArrayList(synths));
-
-		// Select current synthesizer
-		try (Synthesizer currentSynth = MidiSystem.getSynthesizer()) {
-			if (!synths.contains(currentSynth.getDeviceInfo())) {
-				logger.warn("List of synths does not contain the current one");
-			}
-			synth.getSelectionModel().select(currentSynth.getDeviceInfo());
-		} catch (MidiUnavailableException e) {
-			logger.error("Could not obtain current synthesizer", e);
-		}
 
 		// Set column values
 		synthNameColumn.setCellValueFactory(f -> new SimpleStringProperty(
@@ -71,21 +54,6 @@ public class MidiSettingsController {
 				f.getValue().getDescription()));
 		synthVersionColumn.setCellValueFactory(f -> new SimpleStringProperty(
 				f.getValue().getVersion()));
-	}
-
-	private List<MidiDevice.Info> synthesizers() {
-		List<MidiDevice.Info> result = new ArrayList<>();
-		for (MidiDevice.Info info : MidiSystem.getMidiDeviceInfo()) {
-			logger.debug("MIDI device: {}", info);
-			try (MidiDevice device = MidiSystem.getMidiDevice(info)) {
-				if (device instanceof Synthesizer) {
-					result.add(info);
-				}
-			} catch (MidiUnavailableException e) {
-				logger.error("Could not obtain device " + info, e);
-			}
-		}
-		return result;
 	}
 
 }
