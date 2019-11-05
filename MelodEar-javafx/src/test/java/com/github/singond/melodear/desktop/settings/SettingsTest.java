8ecb4dbf2fde5dfc1378e684a9ee215dda11e651
Jan Singon Slany <singond@seznam.cz>
2019-11-05 22:23:56 +0100

    Remove deprecated method call from test

--- MelodEar-javafx/src/test/java/com/github/singond/melodear/desktop/settings/SettingsTest.java
+++ MelodEar-javafx/src/test/java/com/github/singond/melodear/desktop/settings/SettingsTest.java
@@ -35,16 +35,18 @@ public class SettingsTest {
 	@Test
 	public void mutableValue() {
 		MockSettings src = new MockSettings();
-		Date dateExp;
+		Date dateExp, dateNew;
 		try {
 			src.setDate(dateFmt.parse("15.03.2016"));
 			dateExp = dateFmt.parse("15.03.2016");
+			dateNew = dateFmt.parse("15.03.2014");
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
 		MockSettings copy = src.copy();
-		assertEquals("Property 'date' not copied correctly", dateExp, copy.getDate());
-		src.getDate().setYear(114);
+		assertEquals("Property 'date' not copied correctly",
+				dateExp, copy.getDate());
+		src.getDate().setTime(dateNew.getTime());
 		assertEquals("Property 'date' not protected from changes to original",
 				dateExp, copy.getDate());
 	}
