4fef32353dd266679502b15da5ebd900b2668e50
Jan Singon Slany <singond@seznam.cz>
2019-11-14 00:02:10 +0100

    Update PropertyAbstractSettingsTree

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/settings/PropertyAbstractSettingsTree.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/settings/PropertyAbstractSettingsTree.java
@@ -3,11 +3,15 @@ package com.github.singond.melodear.desktop.settings;
 import java.util.function.Function;
 
 import javafx.beans.property.Property;
+import javafx.util.StringConverter;
 
+import com.github.singond.settings.AbstractSettingsNode;
 import com.github.singond.settings.AbstractSettingsTree;
-import com.github.singond.settings.AbstractSettingsValue;
+import com.github.singond.settings.SettingsNodeVisitor;
+import com.github.singond.settings.SettingsValueNode;
 
-public abstract class PropertyAbstractSettingsTree<S extends PropertyAbstractSettingsTree<S>>
+public abstract class PropertyAbstractSettingsTree
+		<S extends PropertyAbstractSettingsTree<S>>
 		extends AbstractSettingsTree<S> {
 
 	public PropertyAbstractSettingsTree(String key) {
@@ -15,27 +19,34 @@ public abstract class PropertyAbstractSettingsTree<S extends PropertyAbstractSet
 	}
 
 	protected <T, P extends Property<T>> P newPropertyNode(String key,
-			P property, Function<T, T> propertyValueDuplicator) {
-		super.newNode(new PropertySettingsValue<T>(key, property, propertyValueDuplicator));
+			P property, Function<T, T> propertyValueDuplicator,
+			StringConverter<T> stringConverter) {
+		super.newNode(new PropertySettingsValue<T>(key, property,
+				propertyValueDuplicator, stringConverter));
 		return property;
 	}
 
 	protected <T, P extends Property<T>> P newPropertyNode(String key,
-			P property) {
-		return newPropertyNode(key, property, Function.identity());
+			P property, StringConverter<T> stringConverter) {
+		return newPropertyNode(key, property, Function.identity(),
+				stringConverter);
 	}
 
 	private static final class PropertySettingsValue<T>
-			extends AbstractSettingsValue<T, PropertySettingsValue<T>> {
+			extends AbstractSettingsNode<PropertySettingsValue<T>>
+			implements SettingsValueNode<T, PropertySettingsValue<T>> {
 
 		private final Property<T> property;
 		private final Function<T, T> duplicator;
+		private final StringConverter<T> stringConverter;
 
 		public PropertySettingsValue(String key, Property<T> property,
-				Function<T, T> duplicator) {
+				Function<T, T> duplicator,
+				StringConverter<T> stringConverter) {
 			super(key);
 			this.property = property;
 			this.duplicator = duplicator;
+			this.stringConverter = stringConverter;
 		}
 
 		@Override
@@ -59,8 +70,28 @@ public abstract class PropertyAbstractSettingsTree<S extends PropertyAbstractSet
 		}
 
 		@Override
+		public String valueToString() {
+			return stringConverter.toString(value());
+		}
+
+		@Override
+		public T valueFromString(String string) {
+			return stringConverter.fromString(string);
+		}
+
+		@Override
+		public void setValueFromString(String string) {
+			setValue(valueFromString(string));
+		}
+
+		@Override
 		public void updateWith(PropertySettingsValue<T> src) {
 			property.setValue(src.valueCopy());
+		}
+
+		@Override
+		public void invite(SettingsNodeVisitor visitor) {
+			visitor.visitValue(this);
 		}
 	}
 }
