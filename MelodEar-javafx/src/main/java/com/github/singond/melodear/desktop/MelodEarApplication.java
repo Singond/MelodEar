2ca12ff3180d894e36ad4726dae37f7a119ba24b
Jan Singon Slany <singond@seznam.cz>
2020-01-03 23:48:23 +0100

    Add application icon to jar

--- MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/MelodEarApplication.java
+++ MelodEar-javafx/src/main/java/com/github/singond/melodear/desktop/MelodEarApplication.java
@@ -1,13 +1,22 @@
 package com.github.singond.melodear.desktop;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The main class of the application.
@@ -15,6 +24,14 @@ import javafx.stage.Stage;
  * @author Singon
  */
 public class MelodEarApplication extends Application {
+
+	private static final List<String> iconNames = Arrays.asList(
+			"icon-48.png",
+			"icon-256.png"
+			);
+	private static final String iconPrefix = "/img/";
+
+	private static Logger logger = LogManager.getLogger(MelodEarApplication.class);
 
 	private MainComponent component;
 
@@ -32,8 +49,24 @@ public class MelodEarApplication extends Application {
 		Scene scene = new Scene(root);
 
 		stage.setTitle("MelodEar");
+		stage.getIcons().addAll(initIcons());
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	private List<Image> initIcons() {
+		List<Image> icons = new ArrayList<>(iconNames.size());
+		for (String name : iconNames) {
+			try (InputStream iconRes = MelodEarApplication.class
+					.getResourceAsStream(iconPrefix + name)) {
+				if (iconRes != null) {
+					icons.add(new Image(iconRes));
+				}
+			} catch (IOException e) {
+				logger.error("Error opening icon file", e);
+			}
+		}
+		return icons;
 	}
 
 	public static void main(String[] args) {
